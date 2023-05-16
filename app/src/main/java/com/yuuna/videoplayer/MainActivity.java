7e313e6ad6a7c59bfd62ddb1688af0017a0995af
Faris-0 <faris.abdulh@gmail.com>
2023-05-16 14:43:13 +0700

    Initial commit

---
+++ app/src/main/java/com/yuuna/videoplayer/MainActivity.java
@@ -0,0 +1,94 @@
+package com.yuuna.videoplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
+
+public class MainActivity extends Activity {
+
+    private StyledPlayerView styledPlayerView;
+
+    private ExoPlayer exoPlayer;
+    private Uri uri, cUri;
+
+    private Long position;
+    private Boolean doubleBackToExit = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        styledPlayerView = findViewById(R.id.idExoPlayerVIew);
+
+        exoPlayer = new ExoPlayer.Builder(this).build();
+        styledPlayerView.setPlayer(exoPlayer);
+
+        uri = getIntent().getData();
+        if (uri != null) initializePlayer();
+    }
+
+    private void initializePlayer() {
+        runOnUiThread(() -> {
+            exoPlayer.stop();
+            exoPlayer = new ExoPlayer.Builder(this).build();
+            styledPlayerView.setPlayer(exoPlayer);
+            if (uri != null) exoPlayer.addMediaItem(MediaItem.fromUri(uri));
+            exoPlayer.prepare();
+            exoPlayer.setPlayWhenReady(true);
+            if (position != null && uri == cUri) exoPlayer.seekTo(position);
+            exoPlayer.addListener(new Player.Listener() {
+                @Override
+                public void onEvents(Player player, Player.Events events) {
+                    Player.Listener.super.onEvents(player, events);
+                    if (player.getVideoSize().width > player.getVideoSize().height) {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                    } else {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                    }
+                }
+            });
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cUri = uri;
+        position = exoPlayer.getCurrentPosition();
+        exoPlayer.release();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        uri = intent.getData();
+        initializePlayer();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExit) finishAndRemoveTask();
+
+        doubleBackToExit = true;
+        Toast.makeText(this, "Tekan sekali lagi untuk keluar dari aplikasi", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> doubleBackToExit = false, 2000);
+    }
+}
